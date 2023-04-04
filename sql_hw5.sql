f0f303f2214bd64c721bacd8d8713211f8a12031
Merve Sena Çınar <108677888+mervesenacnr@users.noreply.github.com>
2023-04-04 14:42:33 +0300

    Create sql_hw5.sql

---
+++ sql_hw5.sql
@@ -0,0 +1,6 @@
+SELECT title, length FROM film WHERE title LIKE '%n' order by legth LIMIT 5;
+
+SELECT title, length FROM film WHERE title LIKE '%n' ORDER BY length (ASC) OFFSET 2 lımıt 5;
+
+SELECT last_name FROM customer WHERE store_id = 1 ORDER BY last_name (DESC) LIMIT 4;
+
